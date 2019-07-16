f9e15421b1c8d412c4e10f9ba781dd867b265952
eaglesakura <eagle.sakura@gmail.com>
2019-07-16 23:09:56 +0900

    add: LiveDataFactory.transformInto

--- armyknife-jetpack/src/main/java/com/eaglesakura/armyknife/android/extensions/LiveDataFactory.kt
+++ armyknife-jetpack/src/main/java/com/eaglesakura/armyknife/android/extensions/LiveDataFactory.kt
@@ -43,6 +43,390 @@ object LiveDataFactory {
     /**
      * 1 live data to 1 data.
      */
+    fun <A, T> transformInto(
+        a: LiveData<A>,
+        inject: (dst: MutableLiveData<T>, a: A) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(result, a.value ?: return@addSource)
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, T> transformNullableInto(
+        a: LiveData<A>,
+        inject: (dst: MutableLiveData<T>, a: A?) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(result, a.value)
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, T> transformInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        inject: (dst: MutableLiveData<T>, a: A, b: B) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, T> transformNullableInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        inject: (dst: MutableLiveData<T>, a: A?, b: B?) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value,
+                    b.value
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value,
+                    b.value
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, C, T> transformInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        c: LiveData<C>,
+        inject: (dst: MutableLiveData<T>, a: A, b: B, c: C) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource
+                )
+            }
+            result.addSource(c) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, C, T> transformNullableInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        c: LiveData<C>,
+        inject: (dst: MutableLiveData<T>, a: A?, b: B?, c: C?) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value
+                )
+            }
+            result.addSource(c) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, C, D, T> transformInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        c: LiveData<C>,
+        d: LiveData<D>,
+        inject: (dst: MutableLiveData<T>, a: A, b: B, c: C, d: D) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource
+                )
+            }
+            result.addSource(c) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource
+                )
+            }
+            result.addSource(d) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, C, D, T> transformNullableInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        c: LiveData<C>,
+        d: LiveData<D>,
+        inject: (dst: MutableLiveData<T>, a: A?, b: B?, c: C?, d: D?) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value
+                )
+            }
+            result.addSource(c) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value
+                )
+            }
+            result.addSource(d) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, C, D, E, T> transformInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        c: LiveData<C>,
+        d: LiveData<D>,
+        e: LiveData<E>,
+        inject: (dst: MutableLiveData<T>, a: A, b: B, c: C, d: D, e: E) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource,
+                    e.value ?: return@addSource
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource,
+                    e.value ?: return@addSource
+                )
+            }
+            result.addSource(c) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource,
+                    e.value ?: return@addSource
+                )
+            }
+            result.addSource(d) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource,
+                    e.value ?: return@addSource
+                )
+            }
+            result.addSource(e) {
+                inject(
+                    result,
+                    a.value ?: return@addSource,
+                    b.value ?: return@addSource,
+                    c.value ?: return@addSource,
+                    d.value ?: return@addSource,
+                    e.value ?: return@addSource
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
+    fun <A, B, C, D, E, T> transformNullableInto(
+        a: LiveData<A>,
+        b: LiveData<B>,
+        c: LiveData<C>,
+        d: LiveData<D>,
+        e: LiveData<E>,
+        inject: (dst: MutableLiveData<T>, a: A?, b: B?, c: C?, d: D?, e: E?) -> Unit
+    ): MutableLiveData<T> {
+        return MediatorLiveData<T>().also { result ->
+            result.addSource(a) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value,
+                    e.value
+                )
+            }
+            result.addSource(b) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value,
+                    e.value
+                )
+            }
+            result.addSource(c) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value,
+                    e.value
+                )
+            }
+            result.addSource(d) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value,
+                    e.value
+                )
+            }
+            result.addSource(e) {
+                inject(
+                    result,
+                    a.value,
+                    b.value,
+                    c.value,
+                    d.value,
+                    e.value
+                )
+            }
+        }
+    }
+
+    /**
+     * 1 live data to 1 data.
+     */
     fun <A, T> transform(
         a: LiveData<A>,
         mapper: (a: A) -> T
