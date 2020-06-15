96ecbf37a2d8e041497adc41124e04a6d559fead
eaglesakura <eagle.sakura@gmail.com>
2020-06-15 09:52:21 +0900

    chg: refactored CircleCI config

--- armyknife-jetpack-dependencies/build.gradle.kts
+++ armyknife-jetpack-dependencies/build.gradle.kts
@@ -1,6 +1,4 @@
 apply(from = "../dsl/android-library.gradle")
-apply(from = "../dsl/ktlint.gradle")
-apply(from = "../dsl/bintray.gradle")
 
 /**
  * Jetpack Libraries.
--- configure.gradle.kts
+++ configure.gradle.kts
@@ -1,31 +1,85 @@
+import com.eaglesakura.armyknife.runtime.extensions.encodeMD5
 import java.nio.charset.Charset
 
-extra["base_version"] = "1.4"
-extra["artifact_name"] = project.name
-extra["artifact_group"] = "com.eaglesakura.armyknife.${extra["artifact_name"]}"
-extra["bintray_user"] = "eaglesakura"
-extra["bintray_labels"] = arrayOf("android", "kotlin")
-extra["bintray_vcs_url"] = "https://github.com/eaglesakura/${project.name}"
+buildscript {
+    repositories {
+        maven(url = "https://dl.bintray.com/eaglesakura/maven/")
+    }
+    dependencies {
+        classpath("com.eaglesakura.armyknife.armyknife-runtime:armyknife-runtime:1.3.7")
+    }
+}
+
+rootProject.extra["artifact_name"] = project.name
+rootProject.extra["artifact_group"] = "com.eaglesakura.armyknife.${rootProject.extra["artifact_name"]}"
+rootProject.extra["bintray_user"] = "eaglesakura"
+rootProject.extra["bintray_labels"] = arrayOf("android", "kotlin")
+rootProject.extra["bintray_vcs_url"] = "https://github.com/eaglesakura/${project.name}"
 
 /**
  * Auto configure.
  */
-extra["artifact_version"] = System.getenv("CIRCLE_TAG").let { CIRCLE_TAG ->
-    val majorMinor = if (CIRCLE_TAG.isNullOrEmpty()) {
-        rootProject.extra["base_version"] as String
-    } else {
-        return@let CIRCLE_TAG.substring(CIRCLE_TAG.indexOf('v') + 1)
-    }
+rootProject.extra["artifact_version"] = when {
+    !System.getenv("CIRCLE_TAG").isNullOrEmpty() -> {
+        System.getenv("CIRCLE_TAG").let { CIRCLE_TAG ->
+            val majorMinor = if (CIRCLE_TAG.isNullOrEmpty()) {
+                rootProject.extra["base_version"] as String
+            } else {
+                return@let CIRCLE_TAG.substring(CIRCLE_TAG.indexOf('v') + 1)
+            }
 
-    val buildNumberFile = rootProject.file(".configs/secrets/build-number.env")
-    if (buildNumberFile.isFile) {
-        return@let "$majorMinor.build-${buildNumberFile.readText(Charset.forName("UTF-8"))}"
-    }
+            val buildNumberFile = rootProject.file(".configs/secrets/build-number.env")
+            if (buildNumberFile.isFile) {
+                return@let "$majorMinor.build-${buildNumberFile.readText(Charset.forName("UTF-8"))}"
+            }
 
-    return@let when {
-        hasProperty("install_snapshot") -> "$majorMinor.99999"
-        System.getenv("CIRCLE_BUILD_NUM") != null -> "$majorMinor.build-${System.getenv("CIRCLE_BUILD_NUM")}"
-        else -> "$majorMinor.snapshot"
+            return@let when {
+                hasProperty("install_snapshot") -> "$majorMinor.99999"
+                System.getenv("CIRCLE_BUILD_NUM") != null -> "$majorMinor.build-${System.getenv("CIRCLE_BUILD_NUM")}"
+                else -> "$majorMinor.snapshot"
+            }
+        }
     }
+    else -> "${rootProject.extra["base_version"] as String}.snapshot"
 }.trim()
 
+val gradleHashFileText = listOf(
+    rootProject.projectDir
+        .listFiles()!!
+        .filter { it.isFile }
+        .filter { it.name.contains(".gradle") },
+    rootProject.projectDir
+        .listFiles()!!
+        .filter { it.isDirectory }
+        .map { it.listFiles()!!.toList() }
+        .flatten()
+        .filter { it.name.contains(".gradle") }
+).asSequence().flatten()
+    .filter {
+        it.isFile && it.name.contains(".gradle")
+    }
+    .toSet()
+    .toList()
+    .sortedWith { a, b -> a.absolutePath.compareTo(b.absolutePath) }.toList()
+    .let { fileList ->
+        val root = rootProject.rootDir
+        buildString {
+            fileList
+                .map { it to it.readText() }
+                .map { it.first to it.second.replace("\n", "").replace("\r", "") }
+                .forEach { (file, text) ->
+                    append(text.toByteArray().encodeMD5())
+                    append(" : ")
+                    append(
+                        file.canonicalPath.substring(root.canonicalPath.length)
+                            .replace("\\", "/")
+                    )
+                    append("\n")
+                }
+        }
+    }
+    .also { hashText ->
+        val dst = rootProject.file(".circleci/hash-gradle")
+        dst.delete()
+        dst.writeText(hashText)
+    }
