330d4ff1c1702a5540b893460c89398a46065744
eaglesakura <eagle.sakura@gmail.com>
2020-12-07 22:22:34 +0900

    add: UriExtensions, ContentResolverExtensions class

--- armyknife-jetpack/src/main/java/com/eaglesakura/armyknife/android/extensions/ContextExtensions.kt
+++ armyknife-jetpack/src/main/java/com/eaglesakura/armyknife/android/extensions/ContextExtensions.kt
@@ -5,12 +5,16 @@ package com.eaglesakura.armyknife.android.extensions
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.ApplicationInfo
+import android.graphics.drawable.Drawable
 import android.net.ConnectivityManager
 import android.os.Build
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.Surface
 import android.view.WindowManager
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 
 @Deprecated(
     "replace to ApplicationRuntime",
@@ -86,4 +90,24 @@ val Context.layoutInflater: LayoutInflater
 fun Context.isConnectedNetwork(): Boolean {
     val service = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     return service.activeNetworkInfo?.isConnected ?: false
+}
+
+/**
+ * Wrap to ContextCompat.getDrawable() method.
+ *
+ * @author @eaglesakura
+ * @link https://github.com/eaglesakura/armyknife-jetpack
+ */
+fun Context.getDrawableCompat(@DrawableRes resId: Int): Drawable? {
+    return ContextCompat.getDrawable(this, resId)
+}
+
+/**
+ * Wrap to ContextCompat.getColor() method.
+ *
+ * @author @eaglesakura
+ * @link https://github.com/eaglesakura/armyknife-jetpack
+ */
+fun Context.getColorCompat(@ColorRes resId: Int): Int {
+    return ContextCompat.getColor(this, resId)
 }
---
+++ armyknife-jetpack/src/main/java/com/eaglesakura/armyknife/android/extensions/UriExtensions.kt
@@ -0,0 +1,22 @@
+package com.eaglesakura.armyknife.android.extensions
+
+import android.net.Uri
+import androidx.core.net.toFile
+import java.io.File
+import java.net.URLDecoder
+
+internal const val SCHEME_FILE = "file://"
+
+/**
+ * Uri to file convert support on Windows path.
+ */
+fun Uri.toFileCompat(): File {
+    val path = this.path ?: ""
+    return if (path.isEmpty()) {
+        // Windows path.
+        val path = toString().substring(SCHEME_FILE.length)
+        File(URLDecoder.decode(path, "UTF-8"))
+    } else {
+        toFile()
+    }
+}
