6c78b0bbc35444756a8e8f339208ce17aac1c10e
eaglesakura <eagle.sakura@gmail.com>
2020-12-07 20:02:47 +0900

    chg: update Dependencies

--- armyknife-jetpack/src/main/java/com/eaglesakura/armyknife/android/extensions/FirebaseRemoteConfigExtensions.kt
+++ armyknife-jetpack/src/main/java/com/eaglesakura/armyknife/android/extensions/FirebaseRemoteConfigExtensions.kt
@@ -4,37 +4,7 @@ package com.eaglesakura.armyknife.android.extensions
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import java.util.concurrent.TimeUnit
-
-@Deprecated(
-    "deprecated access to Firebase.config.developerMode",
-    ReplaceWith("/* FIXME: DELETE THIS */")
-)
-fun FirebaseRemoteConfig.setDebug(set: Boolean) {
-    FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(set).build().let {
-        setConfigSettings(it)
-    }
-}
-
-/**
- * Developer Mode option.
- *
- * @author @eaglesakura
- * @link https://github.com/eaglesakura/armyknife-jetpack
- */
-@Deprecated(
-    "deprecated access to Firebase.config.developerMode",
-    ReplaceWith("/* FIXME: DELETE THIS */")
-)
-var FirebaseRemoteConfig.developerMode
-    get() = info.configSettings.isDeveloperModeEnabled
-    set(value) {
-        setConfigSettings(
-            FirebaseRemoteConfigSettings.Builder()
-                .setDeveloperModeEnabled(value).build()
-        )
-    }
 
 /**
  * fetch remote configs from firebase.
@@ -46,11 +16,6 @@ var FirebaseRemoteConfig.developerMode
  * @link https://github.com/eaglesakura/armyknife-jetpack
  */
 fun FirebaseRemoteConfig.fetch(cacheTime: Long, cacheTimeUnit: TimeUnit): Task<Void> {
-    val cacheTimeSec =
-        if (info.configSettings.isDeveloperModeEnabled) {
-            0
-        } else {
-            cacheTimeUnit.toSeconds(cacheTime)
-        }
+    val cacheTimeSec = cacheTimeUnit.toSeconds(cacheTime)
     return fetch(cacheTimeSec)
 }
--- configure.gradle.kts
+++ configure.gradle.kts
@@ -1,81 +1,30 @@
-import com.eaglesakura.armyknife.runtime.extensions.encodeMD5
 import java.nio.charset.Charset
 
-buildscript {
-    repositories {
-        maven(url = "https://dl.bintray.com/eaglesakura/maven/")
-    }
-    dependencies {
-        classpath("com.eaglesakura.armyknife.armyknife-runtime:armyknife-runtime:1.3.7")
-    }
-}
-
-project.extra["artifact_name"] = project.name
-project.extra["artifact_group"] =
-    "${rootProject.extra["base_group"]}.${project.extra["artifact_name"]}"
-project.extra["bintray_user"] = "eaglesakura"
-project.extra["bintray_labels"] = arrayOf("android", "kotlin")
-project.extra["bintray_vcs_url"] = "https://github.com/eaglesakura/${project.name}"
-
-val buildOnCi = System.getenv("CIRCLE_BUILD_NUM") != null
-val buildTag = System.getenv("CIRCLE_TAG") ?: ""
-val buildNumberFile = rootProject.file(".configs/secrets/build-number.env")
-val buildNumber = when {
-    hasProperty("install_snapshot") -> 99999
-    buildNumberFile.isFile -> buildNumberFile.readText(Charset.forName("UTF-8")).trim().toInt()
-    else -> System.getenv("CIRCLE_BUILD_NUM")?.toInt() ?: 1
-}
+extra["artifact_name"] = project.name
+extra["artifact_group"] = "com.eaglesakura.armyknife.${extra["artifact_name"]}"
+extra["bintray_user"] = "eaglesakura"
+extra["bintray_labels"] = arrayOf("android", "kotlin")
+extra["bintray_vcs_url"] = "https://github.com/eaglesakura/${project.name}"
 
 /**
  * Auto configure.
  */
-project.extra["artifact_version"] = when {
-    !buildOnCi -> "${rootProject.extra["base_version"] as String}.snapshot"
-    buildTag.startsWith("v") -> buildTag.substring(1)
-    else -> "${rootProject.extra["base_version"] as String}.build-${buildNumber}"
+extra["artifact_version"] = System.getenv("CIRCLE_TAG").let { CIRCLE_TAG ->
+    val majorMinor = if (CIRCLE_TAG.isNullOrEmpty()) {
+        rootProject.extra["base_version"] as String
+    } else {
+        return@let CIRCLE_TAG.substring(CIRCLE_TAG.indexOf('v') + 1)
+    }
+
+    val buildNumberFile = rootProject.file(".configs/secrets/build-number.env")
+    if (buildNumberFile.isFile) {
+        return@let "$majorMinor.build-${buildNumberFile.readText(Charset.forName("UTF-8"))}"
+    }
+
+    return@let when {
+        hasProperty("install_snapshot") -> "$majorMinor.99999"
+        System.getenv("CIRCLE_BUILD_NUM") != null -> "$majorMinor.build-${System.getenv("CIRCLE_BUILD_NUM")}"
+        else -> "$majorMinor.snapshot"
+    }
 }.trim()
 
-val gradleHashFileText = listOf(
-    rootProject.projectDir
-        .listFiles()!!
-        .filter { it.isFile }
-        .filter { it.name.contains(".gradle") },
-    rootProject.projectDir
-        .listFiles()!!
-        .filter { it.isDirectory }
-        .map { it.listFiles()!!.toList() }
-        .flatten()
-        .filter { it.name.contains(".gradle") }
-).asSequence().flatten()
-    .filter {
-        it.isFile && it.name.contains(".gradle")
-    }
-    .toSet()
-    .toList()
-    .sortedWith { a, b -> a.absolutePath.compareTo(b.absolutePath) }.toList()
-    .let { fileList ->
-        val root = rootProject.rootDir
-        buildString {
-            fileList
-                .map { it to it.readText() }
-                .map { it.first to it.second.replace("\n", "").replace("\r", "") }
-                .forEach { (file, text) ->
-                    append(text.toByteArray().encodeMD5())
-                    append(" : ")
-                    append(
-                        file.canonicalPath.substring(root.canonicalPath.length)
-                            .replace("\\", "/")
-                    )
-                    append("\n")
-                }
-        }
-    }
-    .also { hashText ->
-        val dst = rootProject.file(".circleci/hash-gradle")
-        dst.delete()
-        dst.writeText(hashText)
-    }
-
-println("====================================== Project Configuration ==============================================")
-println("artifact         : ${project.extra["artifact_version"]}")
-println("===========================================================================================================")
